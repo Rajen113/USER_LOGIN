21b5b68a88d95175efe7cda364729fc8baf6815b
rajen <rajendram9516@gmail.com>
2024-12-28 15:23:39 +0530

    database conneting has been fixed

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnect = async () => {
-  const dbURI =process.env.MONGO_URI  ;
+  const dbURI =process.env.MONGO_URI || "mongodb+srv://rajen113:95163315@myinfo.scd4s.mongodb.net/users?retryWrites=true&w=majority&appName=myInfo";
   console.log(dbURI)
   try {
     await mongoose.connect(dbURI);
